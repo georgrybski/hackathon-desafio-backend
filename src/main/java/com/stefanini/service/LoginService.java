098306522485209c35aaffe396babd28677e0856
Georg Rybski <rybskigeorg@gmail.com>
2023-02-27 19:18:53 -0300

    ~ Removendo Logger desnecessário

--- src/main/java/com/stefanini/service/LoginService.java
+++ src/main/java/com/stefanini/service/LoginService.java
@@ -2,7 +2,6 @@ package com.stefanini.service;
 
 import com.stefanini.dto.RequisicaoDeLogin;
 import com.stefanini.security.ValidadorDeLogin;
-import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -18,8 +17,6 @@ public class LoginService {
 
     public boolean validarLogin(RequisicaoDeLogin requisicaoDeLogin) {
         var jogador = jogadorService.pegarJogadorPorNickname(requisicaoDeLogin.getNickname());
-        LoggerFactory.getLogger(LoginService.class).info("Validando login do jogador: " + jogador.getNickname() + " com senha: " + requisicaoDeLogin.getPassword());
-        LoggerFactory.getLogger(LoginService.class).info("BOolean" + validadorDeLogin.validarLogin(jogador, requisicaoDeLogin.getPassword()));
         return validadorDeLogin.validarLogin(jogador, requisicaoDeLogin.getPassword());
     }
 }
